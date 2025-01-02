9bcc96d977636ae476caee2b7a863933c4693032
VaibhavAPatil <vaibhavpatil.vap@gmail.com>
2025-01-02 15:36:28 +0530

    Configure ReduxToolkit (RTK)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
 import "./App.css";
-import conf from "./conf/conf";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1>MegaBlog Feat. Appwrite</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true); // Corrected typo
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData })); // Dispatch login action if user exists
+        } else {
+          dispatch(logout()); // Dispatch logout action if no user
+        }
+      })
+      .finally(() => {
+        setLoading(false); // Stop loading spinner
+      });
+  }, [dispatch]); // Added dispatch to dependency array
+
+  return !loading ? (
+    <div className=" bg-gray-500 p-5 h-screen flex flex-wrap justify-evenly w-full">
+      <div className="w-full">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
