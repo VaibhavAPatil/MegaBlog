493b56d3a8b1ebf3069e10222e8a59b6372a1039
VaibhavAPatil <vaibhavpatil.vap@gmail.com>
2025-01-03 15:50:53 +0530

    Production grade react components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import Logout from "./Header/Logout";
 
-export { Header, Footer };
+export { Header, Footer, Container, Logo, Logout };
