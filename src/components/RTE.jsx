6a1f66ac89ed0493613a4dc00e8a9a9d513f6e7b
VaibhavAPatil <vaibhavpatil.vap@gmail.com>
2025-01-05 16:06:05 +0530

    RTE.jsx

---
+++ src/components/RTE.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import { plugin } from "postcss";
+import { Controller } from "react-hook-form";
+
+function RTE({ name, control, defaultValue = "", label }) {
+  return <div></div>;
+}
+
+export default RTE;
