6aa5d77234876395f37a8e9fc97b3b949a98e19c
VaibhavAPatil <vaibhavpatil.vap@gmail.com>
2025-01-10 16:05:46 +0530

    Adding Form Slug Values

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -4,7 +4,38 @@ import { plugin } from "postcss";
 import { Controller } from "react-hook-form";
 
 function RTE({ name, control, defaultValue = "", label }) {
-  return <div></div>;
+  return (
+    <div className="w-full">
+      {label && <label className="inline-block mb-1 pl-1">{label}</label>}
+      <Controller
+        name={name || "content"}
+        control={control}
+        defaultValue={defaultValue}
+        render={({ field: { onChange } }) => (
+          <Editor
+            initialValue="{defaultValue}"
+            init={{
+              initialValue: defaultValue,
+              height: 500,
+              menubar: true,
+              plugins: [
+                'advlist autolink lists link image charmap print preview anchor',
+                'searchreplace visualblocks code fullscreen',
+                'insertdatetime media table paste code help wordcount',
+                'emoticons template paste textpattern'
+              ],
+              toolbar: 'undo redo | formatselect | bold italic strikethrough forecolor backcolor | \
+              link image media | alignleft aligncenter alignright alignjustify | \
+              numlist bullist outdent indent | removeformat | addcomment \
+              pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment',
+              content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+            }}
+            onEditorChange={onChange}
+          />
+        )}
+      />
+    </div>
+  );
 }
 
 export default RTE;
--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import Logout from "./Header/Logout";
+import RTE from "./RTE";
 
-export { Header, Footer, Container, Logo, Logout };
+export { Header, Footer, Container, Logo, Logout, RTE };
